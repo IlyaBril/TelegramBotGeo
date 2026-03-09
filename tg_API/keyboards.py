ac68cc02719cfbbfd12d017ae980036e4bc3c406
ilyabrilev@yandex.ru <ilyabrilev@yandex.ru>
2026-03-09 20:13:25 +0300

    requirements update

--- tg_API/keyboards.py
+++ tg_API/keyboards.py
@@ -1,19 +1,5 @@
 from telebot import types
 
-category_buttons_2  = [['entertainment.museum', 'Музеи'],
-                         ['national_park', 'Парки'],
-                         ['entertainment', 'Развлечения'],
-                         ['catering.restaurant', 'Рестораны'],
-                         ['catering.food_court', 'Фудкорты'],
-                         ['leisure.picnic', 'Места для пикника'],
-                         ['accommodation', 'Гостинницы'],
-                         ['parking', 'Парковки'],
-                         ['public_transport', 'Транспорт'],
-                         ['beach', 'Пляжи'],
-                         ['amenity.toilet', 'Туалет'],
-                      ['/help', 'help']
-                      ]
-
 category_buttons  = {
     'entertainment.museum': 'Музеи',
     'national_park': 'Парки',
@@ -34,24 +20,8 @@ class InlineKeyboard:
     @staticmethod
     def get_categoties():
         markup = types.InlineKeyboardMarkup(row_width=2)
-        buttons  = [['entertainment.museum', 'Музеи'],
-                         ['national_park', 'Парки'],
-                         ['entertainment', 'Развлечения'],
-                         ['catering.restaurant', 'Рестораны'],
-                         ['catering.food_court', 'Фудкорты'],
-                         ['leisure.picnic', 'Места для пикника'],
-                         ['accommodation', 'Гостинницы'],
-                         ['parking', 'Парковки'],
-                         ['public_transport', 'Транспорт'],
-                         ['beach', 'Пляжи'],
-                         ['amenity.toilet', 'Туалет'],
-                      ['/help', 'help']
-                      ]
-
-        buttons = category_buttons
-
         buttons = [types.InlineKeyboardButton(text, callback_data=data)
-                   for data, text in buttons.items() ]
+                   for data, text in category_buttons.items() ]
         markup.add(*buttons)
         return markup
 
